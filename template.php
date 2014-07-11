480d94580b1d7b0f4dbb6bcdce3bca3dae756907
Steven Bassett <steven.j.bassett@gmail.com>
2014-07-11 15:48:22 -0700

    refactor(template.php): remove hook call already

--- template.php
+++ template.php
@@ -3,12 +3,3 @@
  * @file
  * Primary pre/preprocess functions and alterations.
  */
-
-/**
- * Override or insert variables into the page template.
- *
- * Implements template_process_page().
- */
-function kalatheme_sauce_process_page(&$variables) {
-  $theme_path = drupal_get_path('theme', 'kalatheme_sauce');
-}
